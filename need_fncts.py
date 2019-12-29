86a07b997c17287a19968334d082830819b1702b
InkasRas <petrov.andreha@gmail.com>
2019-12-29 14:19:46 +0300

    need common functions

---
+++ need_fncts.py
@@ -0,0 +1,15 @@
+import pygame
+import os
+
+
+def load_image(name, clr_key=None):
+    fullname = os.path.join('data', name)
+    img = pygame.image.load(fullname).convert()
+    if clr_key is not None:
+        if clr_key == -1:
+            clr_key = img.get_at((0, 0))
+        img.set_colorkey(clr_key)
+    else:
+        img = img.convert_alpha()
+    return img
+
